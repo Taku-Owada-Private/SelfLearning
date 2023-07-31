d4144be7d9b69e16d46080ee071dcace973ae220
Taku Owada <Taku.Owada@ibm.com>
2023-07-31 19:07:42 +0900

    task

--- 5.5 REST APIs/index.js
+++ 5.5 REST APIs/index.js
@@ -35,14 +35,38 @@ app.post("/get-secret", async (req, res) => {
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
-  const postId = req.body.id;
   const postSecret = req.body.secret;
   const postScore = req.body.score;
+  const body = {
+    secret: postSecret,
+    score: postScore
+  };
+
+  try {
+    const result = await axios.post(API_URL + "/secrets/", body, config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+
 
 });
 
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
+  const postSecret = req.body.secret;
+  const postScore = req.body.score;
+  const body = {
+    secret: postSecret,
+    score: postScore
+  };
+  try {
+    const result = await axios.put(API_URL + "/secrets/" + searchId, body, config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+
   // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
 });
 
